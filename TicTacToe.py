2f9323dbe55225d4f89a6401d8c5ac9f877f1a28
Lucas <lucas.n.tsai@gmail.com>
2025-07-25 14:05:23 +0000

    super duper

---
+++ TicTacToe.py
@@ -0,0 +1,13 @@
+list=[]
+for b in range(0,3):
+    list1=[]
+    for a in range(0,3):
+         list1.append(0)
+         print("|_|",end=" ") 
+    list.append(list1)
+    print("")
+print(list)
+
+c=input("which row do you want? (1,2,3): ")
+d=input("which row do you want? (1,2,3): ")
+
